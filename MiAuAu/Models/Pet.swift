7d527b543bf8c482a4b4d0d3c455a87cfe635fc5
Equipe 12 <equipe12.ioscapacitabr@gmail.com>
2025-05-16 20:21:36 -0300

    first commit

---
+++ MiAuAu/Models/Pet.swift
@@ -0,0 +1,14 @@
+//
+//  Pet.swift
+//  MiAuAu
+//
+//  Created by iredefbmac_21 on 16/05/25.
+//
+import SwiftUI
+
+struct Pet {
+    let name: String
+    let image: String
+    
+    let breed: String
+}
